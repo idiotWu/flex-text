c2182c29576bc2fe66eafa5efb37bbfa23db2d98
Dolphin Wood <dolphin.w.e@gmail.com>
2016-09-27 15:45:00 +0900

    update demo

--- demo/main.js
+++ demo/main.js
@@ -1,12 +1,13 @@
 'use strict';
-var size = document.querySelector('#size');
+var width = document.querySelector('#width');
 var dollar = document.querySelector('#dollar');
 var spacing = document.querySelector('#spacing');
 var container = document.querySelector('.dollar');
 
-var sizeVal = document.querySelector('#size-val');
+var widthVal = document.querySelector('#width-val');
 var dollarVal = document.querySelector('#dollar-val');
 var spacingVal = document.querySelector('#spacing-val');
+var ratioVal = document.querySelector('#ratio-val');
 
 var intPart = document.querySelector('.integer');
 var floatPart = document.querySelector('.float');
@@ -14,7 +15,6 @@ var dotPart = document.querySelector('.dot');
 
 var flexText = new FlexText({
     container: container,
-    styles: { fontFamily: 'Helvetica, Arial, "Hiragino Sans GB", "Microsoft YaHei", "WenQuan Yi Micro Hei", sans-serif' },
     items: [
         {
             elem: document.querySelector('.symbol'),
@@ -45,6 +45,20 @@ function forEach(arr, fn) {
     }
 }
 
+function map(arr, fn) {
+    if (typeof arr.map === 'function') {
+        return arr.map(fn);
+    }
+
+    var res = [];
+
+    for (var i = 0, max = arr.length; i < max; i++) {
+        res.push(fn(arr[i], i, arr));
+    }
+
+    return res;
+}
+
 function attachEvent(elem, type, handler) {
     if (typeof elem.addEventListener === 'function') {
         return elem.addEventListener(type, handler);
@@ -57,25 +71,46 @@ forEach([
     'input',
     'change',
 ], function (type) {
-    attachEvent(size, type, function () {
-        var value = size.value;
+    attachEvent(width, type, function () {
+        var value = width.value;
         container.style.width = value + 'px';
-        sizeVal.textContent = sizeVal.innerText = value;
+        widthVal.textContent = widthVal.innerText = value;
         flexText.update();
     });
     attachEvent(spacing, type, function () {
         var value = spacing.value;
         spacingVal.textContent = spacingVal.innerText = value;
-        flexText.extendStyles({ spacing: value });
+        flexText.setSpacing(value);
         flexText.update();
     });
     attachEvent(dollar, type, function () {
         var value = dollar.value;
         var p = value.split('.');
         dollarVal.textContent = dollarVal.innerText = value;
-        intPart.textContent = intPart.innerText = p[0];
-        floatPart.textContent = floatPart.innerText = p[1];
+        intPart.textContent = intPart.innerText = p[0] || '';
+        floatPart.textContent = floatPart.innerText = p[1] || '';
         dotPart.textContent = dotPart.innerText = p[1] ? '.' : '';
         flexText.update();
+    });
+});
+
+var ratios = document.querySelectorAll('.flex-ratio');
+
+forEach(ratios, function (el) {
+    forEach([
+        'input',
+        'change',
+    ], function (type) {
+        attachEvent(el, type, function () {
+            var vals = map(ratios, function (e, i) {
+                flexText.items[i].flex = parseFloat(e.value);
+
+                return e.value;
+            });
+
+            ratioVal.textContent = ratioVal.innerText = vals.join(':');
+
+            flexText.update();
+        });
     });
 });
